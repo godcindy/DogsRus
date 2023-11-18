fd13c79c61199db64d505fa14025139ae19e335d
godcindy <cindygodoy@pursuit.org>
2023-11-18 17:37:27 -0500

    updated

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # DogsRus
 Dog Purchasing Site
 
+Links to home, family member, account, basket and from logo working links
+media works for smaller screen
--- dog.js
+++ dog.js
@@ -9,17 +9,35 @@
 //         console.error(error);
 //     });
 
-    // <!-- js for toggle menu -->
+// <!-- js for toggle menu -->
 
-        const menuItems = document.getElementById("menuItems");
+const menuItems = document.getElementById("menuItems");
 
-        MenuItems.style.maxHeight = "0px";
+MenuItems.style.maxHeight = "0px";
 
-        function menuToggle() {
-            if (menuItems.style.maxHeight == "0px") {
-                MenuItems.style.maxHeight = "200px";
-            }
-            else{
-                menuItems.style.maxHeight = "0px";
-            }
-        }
+function menuToggle() {
+    if (menuItems.style.maxHeight == "0px") {
+        MenuItems.style.maxHeight = "200px";
+    }
+    else {
+        menuItems.style.maxHeight = "0px";
+    }
+}
+
+// account page this allows indicator/line to move from left to right and give you correct window based on choice/
+const LoginForm = document.getElementById("LoginForm");
+const RegForm = document.getElementById("RegForm");
+const indicator = document.getElementById("indicator");
+
+function register() {
+    RegForm.style.transform = "translateX(0px)";
+    LoginForm.style.transform = "translateX(0px)";
+    indicator.style.transform = "translateX(100px)";
+}
+
+function login() {
+    RegForm.style.transform = "translateX(300px)";
+    LoginForm.style.transform = "translateX(300px)";
+    indicator.style.transform = "translateX(0px)";
+}
+
