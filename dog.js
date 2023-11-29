e6fe616b5b07e764e36df821b34c3fd290459bb0
godcindy <cindygodoy@pursuit.org>
2023-11-28 23:15:02 -0500

    api works

--- dog.js
+++ dog.js
@@ -1,68 +1,64 @@
-// document.addEventListener('DOMContentLoaded', () => {
-
-// })
-
-
-// const quotedisplay = document.querySelector('.quote')
-// const btn = document.querySelector('.btn')
+// const quotedisplay = document.querySelector('.fact-container');
+// const btn = document.querySelector('.btn');
 
 // function getData() {
 
-//     fetch('https://dogapi.dog/api/v2/facts')
+//     fetch('https://dogapi.dog/api/v1/facts')
 //         .then((response) => response.json())
 //         .then((data) => {
-//             console.log(data.body)
 
-//             const fact = data.body
-//             console.log(fact)
-//             quotedisplay.innerHTML = fact
-
+//             const fact = data.facts[0];
+            
+//             quotedisplay.innerHTML = `<p>${fact}</p>`;
+//         })
+//         .catch(error => {
+//             console.error('Error fetching data', error);
 //         })
 
 // }
-// document.getElementById("btn").addEventListener('click', getData)
+// document.addEventListener('click', getData)
 
 
 
 
 
-// <!-- js for toggle menu -->
+<!-- js for toggle menu -->
 
-// const menuItems = document.getElementById("menuItems");
+const menuItems = document.getElementById("menuItems");
 
-// MenuItems.style.maxHeight = "0px";
+MenuItems.style.maxHeight = "0px";
 
-// function menuToggle() {
-//     if (menuItems.style.maxHeight == "0px") {
-//         MenuItems.style.maxHeight = "200px";
-//     }
-//     else {
-//         menuItems.style.maxHeight = "0px";
-//     }
-// }
-
-
+function menuToggle() {
+    if (menuItems.style.maxHeight == "0px") {
+        MenuItems.style.maxHeight = "200px";
+    }
+    else {
+        menuItems.style.maxHeight = "0px";
+    }
+}
 
 
 
 
 
-// account page this allows indicator/line to move from left to right and give you correct window based on choice/
-// const LoginForm = document.getElementById("LoginForm");
-// const RegForm = document.getElementById("RegForm");
-// const indicator = document.getElementById("indicator");
 
-// function register() {
-//     RegForm.style.transform = "translateX(0px)";
-//     LoginForm.style.transform = "translateX(0px)";
-//     indicator.style.transform = "translateX(100px)";
-// }
 
-// function login() {
-//     RegForm.style.transform = "translateX(300px)";
-//     LoginForm.style.transform = "translateX(300px)";
-//     indicator.style.transform = "translateX(0px)";
-// }
+account page this allows indicator/line to move from left to right and give you correct window based on choice/
+const LoginForm = document.getElementById("LoginForm");
+const RegForm = document.getElementById("RegForm");
+const indicator = document.getElementById("indicator");
+
+function register() {
+    RegForm.style.transform = "translateX(0px)";
+    LoginForm.style.transform = "translateX(0px)";
+    indicator.style.transform = "translateX(100px)";
+}
+
+function login() {
+    RegForm.style.transform = "translateX(300px)";
+    LoginForm.style.transform = "translateX(300px)";
+    indicator.style.transform = "translateX(0px)";
+}
 
 
 
